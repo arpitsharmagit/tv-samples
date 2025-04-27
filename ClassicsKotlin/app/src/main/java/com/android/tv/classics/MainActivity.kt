2b2d71bb015bf3a144a184a964551d6f81f3930c
Arpit Sharma <mr.arpit.sharma@hotmail.com>
2025-04-27 15:10:44 +0530

    auto updated commented

--- ClassicsKotlin/app/src/main/java/com/android/tv/classics/MainActivity.kt
+++ ClassicsKotlin/app/src/main/java/com/android/tv/classics/MainActivity.kt
@@ -61,7 +61,7 @@ class MainActivity : FragmentActivity() {
         appUpdateManager.initialize()
         
         // Check for updates
-         checkForAppUpdates()
+//         checkForAppUpdates()
 
         handleIntent(intent)
 
